3d86268cdb213dfd00a3cdff9497d89bb7cd4908
Andy Miller <amcoder@gmail.com>
2025-02-24 14:50:22 -0500

    Create header and navigation components

---
+++ app/components/header.tsx
@@ -0,0 +1,23 @@
+import './header.css'
+
+export function Header() {
+  return (
+    <header id='main-header'>
+      <h1>Libri</h1>
+      <form>
+        <input type='text' placeholder='Search' />
+      </form>
+      <menu>
+        <li>
+          <a href='#'>User</a>
+        </li>
+        <li>
+          <a href='#'>Settings</a>
+        </li>
+        <li>
+          <a href='#'>Help</a>
+        </li>
+      </menu>
+    </header>
+  )
+}
---
+++ app/components/navigation.tsx
@@ -0,0 +1,30 @@
+import './navigation.css'
+import { Link } from '@tanstack/react-router'
+
+export function Navigation() {
+  return (
+    <nav>
+      <menu>
+        <li>
+          <Link to='/books'>Books</Link>
+        </li>
+        <li>
+          <Link to='/books/$id' params={{ id: '1' }}>
+            Book 1
+          </Link>
+        </li>
+        <li>
+          <Link to='/books/$id' params={{ id: '2' }}>
+            Book 2
+          </Link>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+        <li>
+          <a href='#'>About</a>
+        </li>
+      </menu>
+    </nav>
+  )
+}
